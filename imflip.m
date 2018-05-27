b7aa6a6615fa6c8eae44b068e658af7c2b59e36e
YeonjeeJung <jina_1206@naver.com>
2018-05-27 20:22:01 +0900

    Update imflip.m

--- imflip.m
+++ imflip.m
@@ -14,6 +14,10 @@ elseif direction == 'v'
     flipped = flip(grayimage,1); %vertical
 elseif direction == "hv"
     flipped = flip(flip(grayimage,2),1); %horizontal and vertical
+elseif direction == "rr" %rotate right
+    flipped = imrotate(grayimage,-90);
+elseif direction == "rl" %rotate left
+    flipped = imrotate(grayimage,90);
 else
     disp('Fatal Error : argument direction must be integer between 1~3');
 end
