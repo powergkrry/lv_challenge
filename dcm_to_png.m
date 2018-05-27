3bed2e8e0f87b289b66ae17a293d648fa3df33ad
YeonjeeJung <jina_1206@naver.com>
2018-05-27 20:23:40 +0900

    Update dcm_to_png

--- dcm_to_png.m
+++ dcm_to_png.m
@@ -1,13 +1,13 @@
 %%Convert dcm to png
 
 %convert icontour dicom
-dir_list = dir('/hoem04/powergkrry/lv_challenge/data/icontour_txt'); %list of contour files needed to be converted
-mkdir /hoem04/powergkrry/lv_challenge/data/ioriginal_png
+dir_list = dir('/home/yeonjee/lv_challenge/data/icontour_txt'); %list of contour files needed to be converted
+mkdir /home/yeonjee/lv_challenge/data/ioriginal_png
 
 for i = 3:length(dir_list) %1 is ., 2 is .., 3 is content
     image_name_i = dir_list(i).name(1:12);
-    path_dcm_i = strcat('/hoem04/powergkrry/lv_challenge/data/original_dcm/', image_name_i, '.dcm'); %dcm path to load
-    path_image_i = strcat('/hoem04/powergkrry/lv_challenge/data/ioriginal_png/', num2str(i-2), '.png'); %png image path to save
+    path_dcm_i = strcat('/home/yeonjee/lv_challenge/data/original_dcm/', image_name_i, '.dcm'); %dcm path to load
+    path_image_i = strcat('/home/yeonjee/lv_challenge/data/ioriginal_png/', num2str(i-2, '%0.3d'), '.png'); %png image path to save
     
     %load dicom
     dicom_i = dicomread(path_dcm_i);
@@ -19,13 +19,13 @@ for i = 3:length(dir_list) %1 is ., 2 is .., 3 is content
 end
 
 %convert ocontour dicom
-dir_list = dir('/hoem04/powergkrry/lv_challenge/data/ocontour_txt'); %list of contour files needed to be converted
-mkdir /hoem04/powergkrry/lv_challenge/data/ooriginal_png
+dir_list = dir('/home/yeonjee/lv_challenge/data/ocontour_txt'); %list of contour files needed to be converted
+mkdir /home/yeonjee/lv_challenge/data/ooriginal_png
 
 for i = 3:length(dir_list) %1 is ., 2 is .., 3 is content
     image_name_o = dir_list(i).name(1:12);
-    path_dcm_o = strcat('/hoem04/powergkrry/lv_challenge/data/original_dcm/', image_name_o, '.dcm'); %dcm path to load
-    path_image_o = strcat('/hoem04/powergkrry/lv_challenge/data/ooriginal_png/', num2str(i-2), '.png'); %png image path to save
+    path_dcm_o = strcat('/home/yeonjee/lv_challenge/data/original_dcm/', image_name_o, '.dcm'); %dcm path to load
+    path_image_o = strcat('/home/yeonjee/lv_challenge/data/ooriginal_png/', num2str(i-2, '%0.3d'), '.png'); %png image path to save
     
     %load dicom
     dicom_o = dicomread(path_dcm_o);
@@ -37,15 +37,15 @@ for i = 3:length(dir_list) %1 is ., 2 is .., 3 is content
 end
 
 %convert pcontour dicom
-dir_list = dir('/hoem04/powergkrry/lv_challenge/data/pcontour_txt'); %list of contour files needed to be converted
-mkdir /hoem04/powergkrry/lv_challenge/data/poriginal_png
+dir_list = dir('/home/yeonjee/lv_challenge/data/pcontour_txt'); %list of contour files needed to be converted
+mkdir /home/yeonjee/lv_challenge/data/poriginal_png
 
 check_multiple_pcontours = 0;
 image_order = 1;
 for i = 3:length(dir_list) %1 is ., 2 is .., 3 is content
     image_name_p = dir_list(i).name(1:12);
-    path_dcm_p = strcat('/hoem04/powergkrry/lv_challenge/data/original_dcm/', image_name_p, '.dcm'); %dcm path to load
-    path_image_p = strcat('/hoem04/powergkrry/lv_challenge/data/poriginal_png/', num2str(image_order), '.png'); %png image path to save
+    path_dcm_p = strcat('/home/yeonjee/lv_challenge/data/original_dcm/', image_name_p, '.dcm'); %dcm path to load
+    path_image_p = strcat('/home/yeonjee/lv_challenge/data/poriginal_png/', num2str(image_order, '%0.3d'), '.png'); %png image path to save
 
     if i ~= length(dir_list) % if i == length(dir_list), there will be an error at dir_list(i + 1)
         if dir_list(i).name(1:12) == dir_list(i + 1).name(1:12) %check whether there are multiple pcontours or not
