8c15731cd5bdf544354f94577114aa633c88c815
YeonjeeJung <jina_1206@naver.com>
2018-05-30 15:53:29 +0900

    update

--- cal_dice.m
+++ cal_dice.m
@@ -2,6 +2,31 @@ dir_list_out = "/home/yeonjee/lv_challenge/temp/";
 dir_list_truth = "/home/yeonjee/lv_challenge/data/dataset/dataset04/p/test_output/";
 flip = ["h/","v/","hv/","o/","rr/","rl/"];
 
+% %dice
+% similarity = 0;
+% similarity_total = 0;
+% for i = 1:length(flip)
+%     dir_list_out_img = dir(strcat(dir_list_out, flip(i)));
+%     dir_list_truth_img = dir(strcat(dir_list_truth, flip(i), "output_", flip(i)));
+%     
+%     for j = 3:length(dir_list_out_img)
+%        out_img = imread(char(strcat(dir_list_out, flip(i), dir_list_out_img(j).name)));
+%        out_img = logical(out_img(:,:,1));
+%        truth_img = imread(char(strcat(dir_list_truth, flip(i), "output_", flip(i), dir_list_truth_img(j).name)));
+%        truth_img = logical(truth_img);
+%        
+%        similarity = similarity + dice(out_img, truth_img);
+%     end
+%     
+%     similarity / (length(dir_list_out_img) - 2)
+%     similarity_total = similarity_total + similarity;
+%     similarity = 0;
+% end
+% 
+% similarity_total/(length(dir_list_out_img) - 2)/length(flip)
+
+
+%fmeasure
 similarity = 0;
 similarity_total = 0;
 for i = 1:length(flip)
@@ -10,11 +35,11 @@ for i = 1:length(flip)
     
     for j = 3:length(dir_list_out_img)
        out_img = imread(char(strcat(dir_list_out, flip(i), dir_list_out_img(j).name)));
-       out_img = logical(rgb2gray(out_img));
+       out_img = logical(out_img(:,:,1));
        truth_img = imread(char(strcat(dir_list_truth, flip(i), "output_", flip(i), dir_list_truth_img(j).name)));
        truth_img = logical(truth_img);
        
-       similarity = similarity + dice(out_img, truth_img);
+       similarity = similarity + computeFmeasure(out_img, truth_img);
     end
     
     similarity / (length(dir_list_out_img) - 2)
---
+++ computeFmeasure.m
@@ -0,0 +1,25 @@
+function [P,R,F,U]=computeFmeasure(G,H)
+% G - Ground truth segmentation (binary)
+% H - segmentation to evaluate (binary)
+ 
+ 
+ P1=sum(sum( G.*H )) ./ sum(sum(H));
+ R1=sum(sum( G.*H )) ./ sum(sum(G));
+ F1=2*P1*R1/(P1+R1);
+
+ U = sum( sum( G.*H ) ) ./ sum( sum( double( G | H ) ) );
+ 
+ P=P1; R=R1; F=F1;
+ 
+ if 0
+   H=1-H;
+   P2=sum(sum( G.*H )) ./ sum(sum(H));
+   R2=sum(sum( G.*H )) ./ sum(sum(G));
+   F2=2*P2*R2/(P2+R2);
+   
+   if F1>F2
+     P=P1; R=R1; F=F1;
+   else
+     P=P2; R=R2; F=F2;
+   end
+ end
